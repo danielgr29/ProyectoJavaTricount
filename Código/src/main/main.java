3ac811250b68a467f7e948c6e04f5b658ef0a56c
BlackCherryCat <alvarodequinta@gmail.com>
2024-05-12 02:36:18 +0200

    test

--- Código/src/main/main.java
+++ Código/src/main/main.java
@@ -11,7 +11,7 @@ public class main {
 		List<Gasto> gastos = new ArrayList<>();
 		/* Al instanciar un elemento usuario, este tiene que cerrar la base de datos con el
 		 * método conClose que está en la clase Usuario, no creo que pueda ir en un lugar mejor
-		 * y creo que en Grupo y Gasto tendre que hacer lo mismo 
+		 * y creo que en Grupo y Gasto tendre que hacer lo mismo.
 		 */
 	}
 
